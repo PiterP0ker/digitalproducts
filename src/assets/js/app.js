e74ada22101eb1d8a78423d4b64d87849127e702
PiterP0ker <piterp0ker2294@gmail.com>
2020-02-03 15:59:02 +0200

    finish

---
+++ src/assets/js/app.js
@@ -0,0 +1,262 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
+// to the top of the file so if we used`import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+// require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+import './lib/foundation-explicit-pieces';
+
+import './lib/slick.min.js';
+
+$(document).foundation();
+
+let baSlider = $('.ba-slider');
+baSlider.slick({
+	slide: '.ba-slide',
+	dots: true,
+	arrows: false
+});
+let baTeamSlider = $('.ba-team-slider');
+baTeamSlider.slick({
+	slide: '.ba-team-slide',
+	dots: true,
+	arrows: false,
+	slidesToShow: 3
+});
+
+function initBaMap() {
+	// The location of mapCenter
+	let cities = {
+		indonesia: {
+			coords: { lat: -7.931425, lng: 112.6353893 },
+			address: 'Jl. Ikan Piranha Atas No.220'
+		}
+	}
+	let mapCenter = cities.indonesia.coords;
+	// The map, centered at mapCenter
+	let mapEl = document.getElementById('map');
+
+	let mapOptions =
+	{
+		zoom: 14,
+		center: mapCenter,
+		disableDefaultUI: true,
+		styles: [
+			{
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#C7C3D9"
+					}
+				]
+			},
+			{
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#616161"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#bdbdbd"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#eeeeee"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#757575"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e5e5e5"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#757575"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dadada"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#616161"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e5e5e5"
+					}
+				]
+			},
+			{
+				"featureType": "transit.station",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#eeeeee"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#c9c9c9"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#9e9e9e"
+					}
+				]
+			}
+		]
+	};
+	let $baMap = new google.maps.Map(mapEl, mapOptions);
+
+	for (let city in cities) {
+	let marker = new google.maps.Marker({
+		position: cities[city].coords,
+		map: $baMap,
+		icon: "{{root}}assets/img/favicon.png"
+	}
+	);
+}
+
+//move map center on select change
+let citySelect = document.querySelector('[data-city-select]');
+
+citySelect.addEventListener('change', () => {
+	let city = citySelect.value;
+	let newCoords = cities[city].coords;
+	$baMap.setCenter(newCoords);
+
+});
+}
+
+
+window.addEventListener('load', initBaMap);
